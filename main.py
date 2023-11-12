c69e6ff5ac6ef976a44f35515ce796fb2fe2c310
dongil <dikwon79@naver.com>
2023-11-13 03:25:58 +0900

    remoteok.com added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from extractors.remoteok import extract_remoteok_jobs
 from file import save_to_file
 
 
@@ -24,7 +25,8 @@ def search():
     else:
         indeed = extract_indeed_jobs(keyword)
         wwr = extract_wwr_jobs(keyword)
-        jobs = indeed + wwr
+        remoteok = extract_remoteok_jobs(keyword)
+        jobs = indeed + wwr +remoteok
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
 
